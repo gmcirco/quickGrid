4ca78ef7e38753f2e34c851c4f5a652f7c09a647
Giovanni Circo <gmcirco42@gmail.com>
2021-06-01 10:41:41 -0400

    Adding additional changes.

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Crimes and Spatial Risk Factors for Hartford, CT
+#'
+#' A dataset containing robberies in Hartford CT for the years
+#' 2018 - 2019. Spatial risk factors and city boundaries are included.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://openhartford-hartfordgis.opendata.arcgis.com/datasets/b226933a414046c498764d3b6821826f_2/explore}
+"hartford_data"
--- R/lgbm_fit.R
+++ R/lgbm_fit.R
@@ -1,7 +1,7 @@
 #----------------------------------------------------#
 # LGBM_FIT
 #----------------------------------------------------#
-#' Fit `lightgbm` model
+#' Fit a `lightgbm` model
 #' 
 #' This is a wrapper around the `lightgbm` function. This function provides a number of conveniences to speed up model building. 
 #' Users should provide data prepared using the 'prep_data' function. 'lgmb_fit' attempts to fit a reasonable model using some default
@@ -108,7 +108,7 @@ lgbm_fit <- function(prep_data,
   # or default\user-specified options
   if(cv == TRUE){
     print("Fitting lgbm model via cross validation...")
-    gbm.fit <- lgbm_fit_cv(dtrain = dtrain,
+    gbm.fit <- .lgbm_fit_cv(dtrain = dtrain,
                            param = model_params,
                            num_leaves = cv.nleaves,
                            learning_rate = cv.lrate,
@@ -198,4 +198,63 @@ lgbm_fit <- function(prep_data,
       scale_color_viridis_c() +
       theme_void()
   )
+}
+
+
+# Option to fit model using cross-validation
+# allow user to specify a range of values for hyperparmeters
+# then select the 'best' model given these parmaeters
+
+.lgbm_fit_cv <- function(dtrain, param, 
+                        num_leaves,
+                        learning_rate,
+                        nrounds){
+  
+  # MODEL TUNING
+  tuning_grid <- expand.grid(
+    num_leaves = num_leaves,
+    learning_rate = learning_rate,
+    nrounds = nrounds
+  )
+  
+  # CROSS VALIDATION TUNING
+  cv.list <- list()
+  for(i in 1:nrow(tuning_grid)){
+    
+    cv.params <- paste0("num_leaves: ", as.numeric(tuning_grid[i,][1]),
+                        " learning_rate: ", as.numeric(tuning_grid[i,][2]),
+                        " nrounds: ", as.numeric(tuning_grid[i,][3])) 
+    print(cv.params)
+    
+    suppressWarnings(
+      cv.out <-
+        lgb.cv(data = dtrain,
+               params = param,
+               nrounds = as.numeric(tuning_grid[i,][3]),
+               num_leaves = tuning_grid[i,][1],
+               learning_rate = tuning_grid[i,][2],
+               verbose = 0,
+               force_col_wise=TRUE,
+               nfold = 5)
+    )
+    
+    cv.list[[i]] <- c('score' = cv.out$best_score,
+                      'num_leaves' = as.numeric(tuning_grid[i,][1]),
+                      'learning_rate' = as.numeric(tuning_grid[i,][2]),
+                      'nrounds' = as.numeric(tuning_grid[i,][3]))
+  }
+  
+  best_model <- Reduce(min, cv.list[1])
+  
+  # Fit model using optimal parameters
+  gbm.fit.cv <- lightgbm(
+    data = dtrain,
+    params = param,
+    num_leaves = best_model[[2]],
+    learning_rate = best_model[[3]],
+    nrounds = best_model[[4]],
+  )
+  
+  print(best_model)
+  return(gbm.fit.cv)
 }
